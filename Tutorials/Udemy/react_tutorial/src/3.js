dc04963d281e78c2bbac490d76c59de04688bcc7
Your Name <you@example.com>
2018-06-11 23:26:55 +0600

    Practice

--- Tutorials/Udemy/react_tutorial/src/3.js
+++ Tutorials/Udemy/react_tutorial/src/3.js
@@ -3,46 +3,7 @@ import React, { Component } from 'react';
 //import './App.css';
 
 import Counter from "./Components/3/Counter"
- /*
-// App obkect
-const app= {
-  title : "My Title",
-  subtitle : "Sub-Title",
-  option : ['One', 'Two', 'Three']
-
-}
-// It is used to check if subtitle exists
-const app2= {
-  title : "My Title",
-  subtitle : undefined,
-  option : ['One', 'Two', 'Three']
-
-}
-// It is used to check if option.length>0
-const app3= {
-  title : "Indecison App",
-  subtitle : "Put your life in the hand of Computer",
-  option : []
-
-}
-
-// It is used to check to Check Both task above
-const app4= {
-  title : "Indecison App",
-  subtitle : undefined,
-  option : []
-}
-
-// This Function Checks If an user location exists or not
-function getLocation(location){
-  if(location){
-    return location
-  }
-  else {
-    return undefined
-  }
-
-}*/
+ 
 // Main Class that renders the Web Application
 class App extends Component {
    count=9
