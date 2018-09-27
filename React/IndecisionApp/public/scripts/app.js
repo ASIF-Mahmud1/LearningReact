0d0260b2e69712078f79f26209f1ddbc20d3b475
Your Name <you@example.com>
2018-09-28 01:19:47 +0600

    ood

--- React/IndecisionApp/public/scripts/app.js
+++ React/IndecisionApp/public/scripts/app.js
@@ -2,12 +2,30 @@
 
 console.log('App is running');
 // JSX - JavaScript XML
-var template = React.createElement(
-  'p',
+var template2 = React.createElement(
+  'div',
   null,
-  'InDecision Application!'
+  React.createElement(
+    'h1',
+    null,
+    'My Name is Asif'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'I am 12 years olf'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'I am From Bangladesh'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'I Feel Good!'
+  )
 );
-
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- React/IndecisionApp/src/app.js
+++ React/IndecisionApp/src/app.js
@@ -1,7 +1,13 @@
 console.log(`App is running`);
 // JSX - JavaScript XML
- var template = <p>InDecision Application!</p>;
-
+ var template2= (
+   <div>
+     <h1>My Name is Asif</h1>
+     <p>I am 12 years olf</p>
+     <p>I am From Bangladesh</p>
+     <p>I Feel Good!</p>
+   </div>
+ );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
