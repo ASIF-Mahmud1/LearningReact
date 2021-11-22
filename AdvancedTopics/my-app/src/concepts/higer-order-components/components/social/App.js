1fa32a58da8bfba1ba678e2e5c385cc68b19dd20
Asif Mahmud <1421015@iub.edu.bd>
2021-11-22 15:47:55 +0600

    HOC in functionianl component

--- AdvancedTopics/my-app/src/concepts/higer-order-components/components/social/App.js
+++ AdvancedTopics/my-app/src/concepts/higer-order-components/components/social/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import Like from './Like'
-import Comment from './Comment'
-const App=()=>{
-    return(
-        <> 
-          <Like/> 
-          <Comment/>
-        
-        </>
-    )
-}
-export default App
+import { useEffect, useState } from "react";
+
+const withData = (Component) => () => {
+  const [results, setResults] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = await response.json();
+    setResults(data.results);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <Component results={results} />;
+};
+
+const ListResults = ({ results }) => {
+  return (
+    <ul>
+      {results.map((pokemon) => (
+        <li key={pokemon.name}>{pokemon.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default withData(ListResults);
