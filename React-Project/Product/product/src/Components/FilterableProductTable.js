d9ae73df2828ad5ee03e31f02a70712db26d925d
Asif Mahmud <1421015@iub.edu.bd>
2021-11-22 15:55:34 +0600

    cleanup

--- React-Project/Product/product/src/Components/FilterableProductTable.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class FilterableProductTable extends React.Component{
-  render(){
-    return(
-           <div>FilterableProductTable </div>
-    );
-  }
-}
-export default FilterableProductTable;
--- React/IndecisionApp/src/app.js
+++
@@ -1,7 +0,0 @@
-console.log(`App is running`);
-// JSX - JavaScript XML
- var template = <p>InDecision App!</p>;
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
